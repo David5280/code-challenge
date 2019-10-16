62bf8a29d81b48928a010d767c19e382577d7e22
David Engel <Davidjengel2012@gmail.com>
2019-10-16 14:32:44 -0600

    Begin builing calculator

---
+++ src/components/Calculator.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="calculator">
+    <input type='number' placeholder='Total bill'/>
+    <select name='Tip Percentage'>
+      <option value='5%'>5%</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .calculator {
+    background-color: lightgrey;
+    width: 400px;
+    height: 500px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+  }
+  input {
+    width: 300px;
+    height: 25px;
+    padding: 5px;
+  }
+</style>
