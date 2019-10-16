bc79dfd97e422872294cab3f02891bd37fa0a3d9
David Engel <Davidjengel2012@gmail.com>
2019-10-16 16:42:02 -0600

    Limit results to two decimal places

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,9 @@ export default {
       const tipAmount = billInfo.totalBill * billInfo.tipPercentage;
       const finalBillAmount = parseInt(billInfo.totalBill) + parseInt(tipAmount);
       const pricePerPerson = finalBillAmount / billInfo.partySize;
-      this.results.tipAmount = tipAmount;
-      this.results.finalBillAmount = finalBillAmount;
-      this.results.pricePerPerson = pricePerPerson;
+      this.results.tipAmount = tipAmount.toFixed(2);
+      this.results.finalBillAmount = finalBillAmount.toFixed(2);
+      this.results.pricePerPerson = pricePerPerson.toFixed(2);
     }
   },
   data() {
