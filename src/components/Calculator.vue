87e1ed9732615b918108f83bf8b4d4c53a3eaf31
David Engel <Davidjengel2012@gmail.com>
2019-10-16 14:45:28 -0600

    Change styling on calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="calculator">
-    <input type='number' placeholder='Total bill'/>
-    <select name='Tip Percentage'>
+    <input 
+      class='calculator-input' 
+      type='number' 
+      placeholder='Total bill'
+    />
+    <select class='calculator-input' name='Tip Percentage'>
       <option value='5%'>5%</option>
+      <option value='10%'>10%</option>
+      <option value='15%'>15%</option>
+      <option value='20%'>20%</option>
+      <option value='25%'>25%</option>
+      <option value='30%'>30%</option>
+      <option value='35%'>35%</option>
+      <option value='40%'>40%</option>
+      <option value='50%'>50%</option>
     </select>
+    <input class='calculator-input' type='text' placeholder='Party Size' />
+    <button class='calculator-submit'>Calculate</button>
   </div>
 </template>
 
@@ -25,10 +39,18 @@ export default {
     padding: 20px;
     display: flex;
     flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
   }
-  input {
-    width: 300px;
-    height: 25px;
-    padding: 5px;
+  .calculator-input {
+    width: 400px;
+    height: 35px;
+    border-radius: 5px;
+  }
+  .calculator-submit {
+    width: 150px;
+    height: 35px;
+    margin-left: auto;
+    border-radius: 5px;
   }
 </style>
