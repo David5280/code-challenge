626ad710ffc4b00f2fa30006229e32ed79521597
David Engel <Davidjengel2012@gmail.com>
2019-10-17 10:15:02 -0600

    Test methods in App.vue

--- src/App.test.js
+++ src/App.test.js
@@ -10,7 +10,44 @@ describe('App', () => {
     const wrapper = mount(App);
     expect(wrapper.vm.$el).toMatchSnapshot();
   });
-  test('calculateTip', () => {
-    
-  })
+  test('calculateTip accurately calculates tip', () => {
+    const wrapper = mount(App);
+    const billInfo = {
+      totalBill: '1000',
+      tipPercentage: '.20',
+      partySize: '',
+    };
+    wrapper.vm.calculateTip(billInfo);
+    expect(wrapper.vm.results.tipAmount).toEqual('200.00');
+  });
+  test('calculateTip accurately calculates finalBill', () => {
+    const wrapper = mount(App);
+    const billInfo = {
+      totalBill: '1000',
+      tipPercentage: '.20',
+      partySize: '',
+    };
+    wrapper.vm.calculateTip(billInfo);
+    expect(wrapper.vm.results.finalBillAmount).toEqual('1200.00');
+  });
+  test('calculateTip accurately calculates pricePerPerson', () => {
+    const wrapper = mount(App);
+    const billInfo = {
+      totalBill: '1000',
+      tipPercentage: '.20',
+      partySize: '2',
+    };
+    wrapper.vm.calculateTip(billInfo);
+    expect(wrapper.vm.results.pricePerPerson).toEqual('600.00');
+  });
+  test('determineTheme produces the correct theme based on tip amount', () => {
+    const wrapper = mount(App);
+    const billInfo = {
+      totalBill: '1000',
+      tipPercentage: '.40',
+      partySize: '2',
+    };
+    wrapper.vm.determineTheme(billInfo.tipPercentage);
+    expect(wrapper.vm.theme).toEqual('theme3');
+  });
 });
