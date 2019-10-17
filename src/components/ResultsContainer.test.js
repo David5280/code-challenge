aa9fb817da51b334f697e3f584312aab20e9d20b
David Engel <Davidjengel2012@gmail.com>
2019-10-16 23:40:42 -0600

    Second test passing

--- src/components/ResultsContainer.test.js
+++ src/components/ResultsContainer.test.js
@@ -3,7 +3,14 @@ import ResultsContainer from './ResultsContainer';
 
 describe('Component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(ResultsContainer)
+    const wrapper = mount(ResultsContainer, {
+      propsData: {
+        results: {
+          tipAmount: '.20',
+          finalBillAmount: '1000'
+        }
+      }
+    })
     expect(wrapper.isVueInstance()).toBeTruthy()
   });
 });
